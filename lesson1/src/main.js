0e01998e01dcb397b0f9989a0a429d487396ab0c
ejiro osiephri <osiephriejiro765@gmail.com>
2024-07-14 10:29:43 +0100

    Refresher

--- lesson1/src/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let Ejiro = 2;
-console.log(Ejiro);
-const a = 2;
-const b = 3;
-const c = "4";
--- lesson1/src/main.ts
+++
@@ -1,6 +0,0 @@
-let Ejiro = 2;
-console.log(Ejiro);
-
-const a: number = 2;
-const b: number = 3;
-const c: string = "4";
